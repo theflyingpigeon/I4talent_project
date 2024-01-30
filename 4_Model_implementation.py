e13431600fad5f68013f50c231af7315d64cb9c7
shaemesuister <shaeme.suister@momomedical.com>
2024-01-30 13:31:01 +0100

    Updated checking algorithm

--- 4_Model_implementation.py
+++ 4_Model_implementation.py
@@ -13,14 +13,22 @@ column_order = ['leeftijd_begin_dienst', 'reisafstand', 'dienstperiode', 'aantal
                 'business_unit_Intern']
 
 
+def create_input_file() -> None:
+    columns: list[str] = ['name']
+    for column in column_order:
+        columns.append(column)
+    column_dataframe: pd.DataFrame = pd.DataFrame(columns=columns)
+    column_dataframe.to_excel('Predictions/Sample_input_file.xlsx', index=False)
+
+
 def check_file() -> bool:
     # Check if someone passed in a potential filename and if it is a file
     try:
         if os.path.exists(argv[1]):
-            if argv[1].lower().endswith('.csv'):
+            if argv[1].lower().endswith('.csv') or argv[1].lower().endswith('xlsx') or argv[1].lower().endswith('xls'):
                 return True
             else:
-                exit('Please use a CSV file')
+                exit('Please use a CSV, XLSX or XLS file')
         else:
             return False
     except IndexError:
@@ -32,34 +40,52 @@ def check_columns(check_df: pd.DataFrame) -> pd.DataFrame:
     check_df = check_df.reindex(columns=column_order, fill_value=False)
 
     # Remove the dienstperiode column
+    check_df = check_df.drop('name', axis=1, errors='ignore')
     check_df = check_df.drop('dienstperiode', axis=1, errors='ignore')
 
     return check_df
 
 
-if __name__ == '__main__':
-    # Check if the file exist
-    if not check_file():
-        exit("Could not find the provided csv file")
-
+def run_prediction() -> None:
     # Set the df variable equal to all the data in the csv file
-    df: pd.DataFrame = pd.read_csv(argv[1])
-    df = check_columns(df)
+    df: pd.DataFrame
+
+    # Read the different filetypes
+    if argv[1].lower().endswith('csv'):
+        df = pd.read_csv(argv[1])
+    elif argv[1].lower().endswith('xlsx') or argv[1].lower().endswith('xls'):
+        df = pd.read_excel(argv[1])
+    else:
+        exit("Could not read file")
+
+    predicted_df = check_columns(df)
 
     # Load the model and make a prediction
     xgb_model = pickle.load(open('models/XGBoost.sav', 'rb'))
-    predictions = xgb_model.predict(df)
-    df['Predicted working period'] = predictions
+    predictions = xgb_model.predict(predicted_df)
+    predicted_df['Predicted working period'] = predictions
+
+    try:
+        predicted_df['name'] = df['name']
+    except IndexError:
+        pass
 
     # Remove the unwanted/unneeded cells
-    columns_to_drop: list[str] = ['afdeling_Accountant', 'afdeling_Administratief medewerker', 'afdeling_BI',
-                'afdeling_Boekhouder', 'afdeling_Business analist', 'afdeling_Business controller',
-                'afdeling_Business development', 'afdeling_Credit controller', 'afdeling_Financial controller',
-                'afdeling_HR', 'afdeling_IT', 'afdeling_Legal', 'afdeling_Marketing',
-                'afdeling_Office manager', 'afdeling_Project controller', 'business_unit_Detachering',
-                'business_unit_Intern']
-    df.drop(columns=columns_to_drop, inplace=True, errors='ignore')
+    predicted_df.drop(columns=column_order, inplace=True, errors='ignore')
+    predicted_df = predicted_df.reindex(columns=['name', 'Predicted working period'])
 
     # Save the working period to a csv and Excel file
-    df.to_csv(f'Predictions/predicted {datetime.now().day} - {datetime.now().month}.csv', sep=',', index=False)
-    df.to_excel(f'Predictions/predicted.xlsx', sheet_name=f'{datetime.now().day} - {datetime.now().month}', index=False)
+    predicted_df.to_csv(f'Predictions/predicted {datetime.now().day} - {datetime.now().month}.csv', sep=',',
+                        index=False)
+    predicted_df.to_excel(f'Predictions/predicted.xlsx', sheet_name=f'{datetime.now().day} - {datetime.now().month}',
+                          index=False)
+
+
+if __name__ == '__main__':
+    if check_file():
+        run_prediction()
+    elif argv[1] == '':
+        create_input_file()
+        print("A template has been created")
+    else:
+        exit('Please input a CSV, XLSX or XLS file')
